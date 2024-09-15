7c73ac3bd2de8678723c495d6c861c0fefe896bf
marcelmarais <marcelmarais2000@gmail.com>
2024-09-15 21:58:25 +0200

    feat: add environment variable setup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+from config.env import env
+
+
 def add(a: int, b: int) -> int:
     return a + b
 
 
 if __name__ == "__main__":
     add(1, 2)
+    print(f"This is an example environment variable: {env.ENV_VAR_1}")
--- test/unit/test_add.py
+++ test/unit/test_add.py
@@ -1,4 +1,4 @@
-from src.main import add
+from main import add
 
 
 def test_add() -> None:
